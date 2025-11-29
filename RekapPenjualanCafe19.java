908d9189c64fc3b7df1cf3214229ba081d029d0b
radadevitasari <radadevitasari87.com>
2025-11-29 21:24:34 +0700

    modifikasi tugas no 3 b

--- RekapPenjualanCafe19.java
+++ RekapPenjualanCafe19.java
@@ -1,72 +1,81 @@
+import java.util.*;
 public class RekapPenjualanCafe19 {
 
-    static String[] menu = {
-        "Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"
-    };
+    static String[] menu;
+    static int[][] penjualan;
 
-    static int[][] penjualan = new int[5][7];
+    public static void inputData(Scanner sc) {
+        System.out.print("Jumlah menu: ");
+        int jmlMenu = sc.nextInt();
+        System.out.print("Jumlah hari: ");
+        int jmlHari = sc.nextInt();
+        sc.nextLine(); 
 
-    public static void inputData() {
-        int[][] data = {
-            {20, 20, 25, 20, 10, 60, 10},
-            {30, 80, 40, 10, 15, 20, 25},
-            {5,  9,  20, 25, 10, 5,  45},
-            {50, 8,  17, 18, 10, 30, 6},
-            {15, 10, 16, 15, 10, 10, 55}
-        };
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 7; j++) {
-                penjualan[i][j] = data[i][j];
+        menu = new String[jmlMenu];
+        penjualan = new int[jmlMenu][jmlHari];
+
+        for (int i = 0; i < jmlMenu; i++) {
+            System.out.print("Nama menu ke-" + (i+1) + ": ");
+            menu[i] = sc.nextLine();
+            for (int j = 0; j < jmlHari; j++) {
+                System.out.print("Penjualan " + menu[i] + " hari ke-" + (j+1) + ": ");
+                penjualan[i][j] = sc.nextInt();
             }
+            sc.nextLine();
         }
     }
     public static void tampilData() {
-        System.out.println("=== Rekap Penjualan 7 Hari ===");
+        System.out.println("\n=== Rekap Penjualan ===");
         System.out.printf("%-15s", "Menu");
-        for (int h = 1; h <= 7; h++) System.out.printf("H%d  ", h);
+
+        for (int h = 1; h <= penjualan[0].length; h++)
+            System.out.printf("H%d  ", h);
         System.out.println();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < menu.length; i++) {
             System.out.printf("%-15s", menu[i]);
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < penjualan[0].length; j++)
                 System.out.printf("%-4d", penjualan[i][j]);
-            }
             System.out.println();
         }
     }
+
     public static void penjualanTertinggi() {
         int maxTotal = -1;
         String maxMenu = "";
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < menu.length; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) total += penjualan[i][j];
+            for (int j = 0; j < penjualan[i].length; j++)
+                total += penjualan[i][j];
+
             if (total > maxTotal) {
                 maxTotal = total;
                 maxMenu = menu[i];
             }
         }
-        System.out.println(
-            "Menu penjualan tertinggi: " + maxMenu + " (Total: " + maxTotal + ")"
-        );
-    }
-    public static void rataRata() {
-        System.out.println("=== Rata-rata Penjualan ===");
-        for (int i = 0; i < 5; i++) {
-            int total = 0;
-            for (int j = 0; j < 7; j++) total += penjualan[i][j];
-            double rata = total / 7.0;
 
+        System.out.println("\nMenu dengan penjualan tertinggi: " + maxMenu +
+                           " (Total: " + maxTotal + ")");
+    }
+
+    public static void rataRata() {
+        System.out.println("\n=== Rata-rata Penjualan ===");
+        for (int i = 0; i < menu.length; i++) {
+            int total = 0;
+            for (int j = 0; j < penjualan[i].length; j++)
+                total += penjualan[i][j];
+
+            double rata = total / (double) penjualan[i].length;
             System.out.println(menu[i] + ": " + rata);
         }
     }
 
     public static void main(String[] args) {
-        inputData();
+        Scanner sc = new Scanner(System.in);
+        inputData(sc);
         tampilData();
         penjualanTertinggi();
         rataRata();
     }
 }
-    
-
